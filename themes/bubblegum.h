be3309636a4c836cc4539dd033fa86a64c164aa0
justcow <68345611+JustCoww@users.noreply.github.com>
2022-03-13 08:16:09 +0000

    Add files via upload

---
+++ themes/bubblegum.h
@@ -0,0 +1,4 @@
+static const char black[]       = "#0f1114";
+static const char blue[]        = "#55CDFC";  // focused window border
+static const char white[]      = "#FFFFFF";
+static const char pink[]        = "#F7A8B8";

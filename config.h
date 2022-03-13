be16eb6dae8f79347dc8e2ef7931a062e0f3a33b
justcow <68345611+JustCoww@users.noreply.github.com>
2022-03-13 08:28:10 +0000

    Update config.h

--- config.h
+++ config.h
@@ -10,7 +10,8 @@ static const unsigned int gappov    = 10;       /* vert outer gap between window
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=14:antialias=true"};
+static const char *fonts[]          = { "FiraMono-Regular:size=14:antialias=true",
+					"JetBrainsMono Nerd Font:size=12:antialias=true"};
 #include "themes/bubblegum.h"
 
 /* Colors */
